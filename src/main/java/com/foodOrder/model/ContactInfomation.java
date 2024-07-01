6adcea72e14bae775864450fa111b263f3e378f4
leminhduy1912 <minhduyle081003@gmail.com>
2024-07-01 20:04:55 +0700

    first commit

---
+++ src/main/java/com/foodOrder/model/ContactInfomation.java
@@ -0,0 +1,11 @@
+package com.foodOrder.model;
+
+import lombok.Data;
+
+@Data
+public class ContactInfomation {
+    private String email;
+    private String mobile;
+    private String twitter;
+    private String instargram;
+}
---
+++ src/main/java/com/foodOrder/model/User.java
@@ -0,0 +1,36 @@
+package com.foodOrder.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.foodOrder.dto.RestaurantDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String fullName;
+    private String email;
+    private USER_ROLE role;
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "customer")
+    private List<Order> orders = new ArrayList<>();
+
+    @ElementCollection
+    private List<RestaurantDto> restaurantFavouriteList = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>();
+
+
+}
